57f852bf23a2fd99d705ce8878459617406fcf35
brandonnarron1-lang <brandonnarron1@gmail.com>
2026-08-18 19:54:58 -0400

    feat: expose canonical growth intelligence view

---
+++ app/lib/growthIntelligenceView.ts
@@ -0,0 +1,11 @@
+export {
+  loadNeonGrowthIntelligence as loadGrowthIntelligence,
+} from "./persistence/neonGrowthIntelligenceView";
+
+export type {
+  GrowthIntelligenceView,
+  PersistedGrowthOpportunity,
+  PersistedGrowthRecommendation,
+  GrowthChannelEconomics,
+  GrowthOpportunity,
+} from "./persistence/neonGrowthIntelligenceView";
